afd804ab7e96ca94eade8206b429e5f909632a16
Rob Mack <robmack8932@icloud.com>
2020-05-14 23:36:21 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheDeck
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# TheDeck
